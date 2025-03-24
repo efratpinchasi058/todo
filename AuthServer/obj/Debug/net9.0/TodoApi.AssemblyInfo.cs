57e80b3396c3086dbc639f160e6add4ce7b728a8
DESKTOP-L9S4R74\This User <ep0583278720@gmail.com>
2025-03-24 19:03:46 +0200

    good21

--- AuthServer/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ AuthServer/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ce35246d832de04181c568fa1f5c7329361b37d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5020018d0c6bdd1043e6a61757b5eaf34057e94")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
