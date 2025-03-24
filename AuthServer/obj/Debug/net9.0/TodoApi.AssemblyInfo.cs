4c8afe6845a01e2981f362c05de63914d969b368
DESKTOP-L9S4R74\This User <ep0583278720@gmail.com>
2025-03-24 19:40:35 +0200

    hashem2

--- AuthServer/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ AuthServer/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebbec70814eaf889ddab3bf7dbc67f0bbbfd4a3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ade97a66041d62f80af1381167a591dde31a9b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
